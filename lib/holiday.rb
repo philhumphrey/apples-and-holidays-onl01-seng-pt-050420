99a4573104bb9b95acb47e22691ba4b1c3de3e05
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-16 20:29:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   
 supplies = []
-holiday_hash[:winter].collect 
+holiday_hash[:winter].collect do |a,b|
+  
   
 
 end
