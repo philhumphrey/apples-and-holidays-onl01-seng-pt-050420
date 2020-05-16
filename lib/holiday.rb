46eca2d412e5692885aea01856b5ef6152e8e184
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-16 19:46:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
 
-holiday_hash[:winter].each do |holiday, decorations|
+holiday_hash[:winter].collect do |holiday, decorations|
   decorations << supply
   
 
